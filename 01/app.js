5ad9202c9acf74d57e1348aee1e9a56773304bdd
Jacek Zielinski <gandi34@wp.pl>
2022-09-07 08:50:49 +0200

    function showTime

--- 01/app.js
+++ 01/app.js
@@ -1,5 +1,6 @@
-function showTime (siema){
-    console.log('Pokaż czas');
+function showTime (){
+    const event = new Date('September 9, 2022 07:46:30 GMT+00:00').toLocaleTimeString('UTC');;
+    console.log(event);
 }
 
-showTime(siema);
+showTime();
